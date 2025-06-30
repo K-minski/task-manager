b1f811336b7b1075c0db75ef3f3ef9af219db655
Dawid Kamiński <dawid.kaminski.code@gmail.com>
2025-06-30 08:21:34 +0200

    Added toString function for TaskStatus

--- src/main/java/dev/dtk/task_manager/Task/TaskStatus.java
+++ src/main/java/dev/dtk/task_manager/Task/TaskStatus.java
@@ -1,9 +1,34 @@
 package dev.dtk.task_manager.Task;
 
 public enum TaskStatus {
-    NOT_STARTED,
-    IN_PROGRESS,
-    COMPLETED,
-    ON_HOLD,
-    CANCELLED,
+    NOT_STARTED {
+        @Override
+        public String toString() {
+            return "Not Started";
+        }
+    },
+    IN_PROGRESS {
+        @Override
+        public String toString() {
+            return "In Progress";
+        }
+    },
+    COMPLETED {
+        @Override
+        public String toString() {
+            return "Completed";
+        }
+    },
+    ON_HOLD {
+        @Override
+        public String toString() {
+            return "On Hold";
+        }
+    },
+    CANCELLED {
+        @Override
+        public String toString() {
+            return "Cancelled";
+        }
+    };
 }

166e23535e9b1870811d4d66995c26f21356ed6e
Dawid Kamiński <dawid.kaminski.code@gmail.com>
2025-07-04 08:07:01 +0200

    Added functionality description update for Task and TaskRepository and improved status update in TaskRepository

--- src/main/java/dev/dtk/task_manager/Task/Task.java
+++ src/main/java/dev/dtk/task_manager/Task/Task.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 
 public class Task {
     private final String id;
-    private final String description;
+    private String description;
     private TaskStatus status;
 
     public Task(String description) {
@@ -19,6 +19,10 @@ public class Task {
 
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public TaskStatus getStatus() {
